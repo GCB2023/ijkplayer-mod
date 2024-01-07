c2161a81e91dccd2df6fe147003ab5f42d48a3d6
root <thesun.lip@gmail.com>
2024-01-07 17:59:33 +0800

    推送

--- init-android-exo.sh
+++ init-android-exo.sh
@@ -16,9 +16,9 @@
 # limitations under the License.
 #
 
-IJK_EXO_UPSTREAM=https://github.com/google/ExoPlayer.git
-IJK_EXO_FORK=https://github.com/google/ExoPlayer.git
-IJK_EXO_COMMIT=r1.5.11
+IJK_EXO_UPSTREAM=https://github.com/GCB2023/ExoPlayer.git
+IJK_EXO_FORK=https://github.com/GCB2023/ExoPlayer.git
+IJK_EXO_COMMIT=r2.19.1
 IJK_EXO_LOCAL_REPO=extra/ExoPlayer
 
 set -e
